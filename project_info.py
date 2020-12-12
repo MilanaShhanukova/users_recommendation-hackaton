8fae4c15d97e02fcbbf3aa9136ed828c5b569fd1
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-12-12 22:35:19 +0300

    Update project_info.py

--- project_info.py
+++ project_info.py
@@ -1,5 +1,6 @@
 import pymongo
 import numpy as np
+import csv
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.mongo_bd
@@ -8,12 +9,13 @@ projects = db.projects
 
 
 class Project:
-    def __init__(self, name: str, sphere: str, r: float, creators: list):
+    def __init__(self, name: str, sphere: str, main_creator, r=0, other_creators=[]):
         self.name = name
         self.sphere = sphere
         self.r = r
-        self.creators = creators
+        self.creators = other_creators
         self.label = 0 # idea - 0, project - 1
+        self.main_creator = main_creator # object user
 
     # add calculated creators
     def add_creators(self, update_creators: list):
@@ -26,7 +28,8 @@ class Project:
         self.r += v_user
 
     def make_idea_project(self):
-        same_projects_rs = [project.r for project in projects.find() if project.sphere == self.sphere]
+        same_projects_rs = [project.r ** (-project.main_creator.v)
+                            for project in projects.find() if project.spheres == self.sphere]
 
-        if self.r >= np.mean(same_projects_rs):
+        if self.r ** (-self.main_creator.v) >= np.mean(same_projects_rs):
             self.label = 1
