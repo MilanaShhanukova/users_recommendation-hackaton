0477b39931f03c907f5d766c7d998a09fc1cc935
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-12-12 22:35:33 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,12 +1,13 @@
 from IPython.display import display
 import pymongo
 from users_info_prepare import Users_info
+import random
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.mongo_bd
 users = db.users_data
 projects = db.projects
-
+users.drop()
 
 def append_user(user_info: dict):
     if user_info not in users.find():
@@ -23,10 +24,22 @@ def get_user(user_name: str):
     return 0
 
 
-one_user = Users_info("Егор Иванов", "программист", "питон")
-one_user.main_info()
+with open("names.txt", encoding="utf-8") as file:
+    users_names = file.read().split('\n')
+with open("professions.txt", encoding="utf-8") as file:
+    professions = file.read().split('\n')
+with open("spheres.txt", encoding="utf-8") as spheres:
+    areas = spheres.read().split('\n')
 
-append_user(one_user.main_info_dict)
+for i in range(len(users_names)):
+    name = users_names[i]
+    prof = professions[i]
+    sphere = areas[i]
+
+    user = Users_info(name, prof, sphere, random.uniform(0, 1))
+    user.main_info()
+    append_user(user.main_info_dict)
+
 for entry in users.find():
     print(entry)
 
