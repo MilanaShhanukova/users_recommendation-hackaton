2775a742565adb9e28871ef6b7bd92f0d11735ed
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-12-13 10:43:00 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -23,14 +23,25 @@ class User_interface:
         except KeyError:
             self.user.spheres.append(sphere_project)
             self.user.interests.append(0.02)
-# testing
 
+# testing
 random_user = Users_info("Иванова Милана", "программист", ["кулинария"], [0.05])
 second_random = Users_info("Пирожкова Милана", "повар", ["кулинария"], [0.04])
-random_project = Project("готовка_с_пирожковой", "программирование", second_random)
 
+random_project = Project("готовка_с_пирожковой", "программирование", second_random)
+random_second_project = Project("готовка_с_пирожковой", "кулинария", second_random)
 
 interface = User_interface(random_user)
 interface.make_project("готовка", "кулинария")
+# check before likes
+print("Check what interest one user has:")
+for int in range(len(random_user.interests)):
+    print(random_user.spheres[int], random_user.interests[int])
 
+print("\nHere one user likes two projects in кулинария and программирование spheres \n")
 interface.like_other_project(random_project)
+interface.like_other_project(random_second_project)
+# after likes
+print("Check what interest one user has now")
+for int in range(len(random_user.interests)):
+    print(random_user.spheres[int], random_user.interests[int])
